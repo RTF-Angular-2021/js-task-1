6a18353087e6d3fb9e5c1603bc0a84fd41eefa37
petr.diukin <petr.diukin@urfu.me>
2021-02-21 17:40:07 +0500

    Дюкин Петр. Задание №1

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -1,12 +1,28 @@
-/** Задача 1 - Сумма цифр числа
-Требуется написать функцию numeralSum, которая
-принимает число,
-требуется вернуть сумму цифр этого числа
-@param {number} number - Исходное число
-@return {number} Сумма цифр числа
- */
+function numeralSum(number) 
+{
+    if (number == Infinity) return Infinity;
+    number = convertNumber(number);
+    var sum = 0;
+    while (number != 0) {
+        sum += number % 10;
+        number = div(number);
+    }
+    return sum;
+}
 
-function numeralSum(number) {
+function convertNumber(number)
+{
+    var processing = String(number);
+    var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var result = "";
+    for (var sym of processing)
+        if (digits.indexOf(sym) != -1) result += sym;
+    return Number(result);
+}
+
+function div(x, sub = 10)
+{
+    return (x - x % sub) / sub;
 }
 
 module.exports.numeralSum = numeralSum;
