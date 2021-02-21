9d59366aaee4e5bae6497a89e95fd0aee806f8ad
petr.diukin <petr.diukin@urfu.me>
2021-02-21 17:44:30 +0500

    Дюкин Петр. task 01

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -13,7 +13,7 @@ function numeralSum(number)
 function convertNumber(number)
 {
     var processing = String(number);
-    var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     var result = "";
     for (var sym of processing)
         if (digits.indexOf(sym) != -1) result += sym;
