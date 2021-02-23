521b0efeff1e8678c2508a81cd60eb4aa80dc989
garche <archersve@gmail.com>
2021-02-23 17:14:40 +0500

    Задача 1 решена

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -4,9 +4,25 @@
 требуется вернуть сумму цифр этого числа
 @param {number} number - Исходное число
 @return {number} Сумма цифр числа
- */
+*/
 
-function numeralSum(number) {
+function numeralSum(number)
+{
+    if (!isFinite(number)){
+        if (isNaN(number)) return 0;
+        return Infinity;
+    }
+
+    let sum = 0;
+    let numberArray = number.toString();
+
+    for (let i = 0; i < numberArray.length; i++) 
+    {
+        if (numberArray[i] == '.' || numberArray[i] == '-') continue;
+        sum += +numberArray[i];
+    }
+
+    return sum;
 }
 
 module.exports.numeralSum = numeralSum;
