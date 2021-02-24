87f860acdb7e46e2583ca9fce661d6259e533417
garche <archersve@gmail.com>
2021-02-24 19:42:03 +0500

    all task complete

--- src/task_2/index.js
+++ src/task_2/index.js
@@ -1,3 +1,4 @@
+"use strict"
 /** Задача 2 - Форматтер для ФИО
 Требуется написать функцию fioFormat, которая
 принимает строку, в которой через пробел написаны три слова: имя, фамилия и отчество,
@@ -5,7 +6,19 @@
 @param {string} inputFio - Строка с именем, фамилией и отчеством
 @return {string} Строка формата Фамилия И.О.
  */
-function fioFormat(inputFio) {
+function fioFormat(inputFio) 
+{
+    let fio = "";
+    for (let char = inputFio.indexOf(" ")+1; char < inputFio.lastIndexOf(" "); char++){
+        fio += inputFio[char];
+    }
+    fio += " ";
+    fio += inputFio[0];
+    fio += ".";
+    let spase = inputFio.lastIndexOf(" ");
+    fio +=inputFio[spase+1];
+    fio += ".";
+    return fio;
 }
 
 module.exports.fioFormat = fioFormat;
