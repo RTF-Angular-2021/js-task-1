4316630d832ea876dd9e6be679878e7228011fee
mikhailbobrov <mikhailurban13@yandex.ru>
2021-08-25 16:53:07 +0500

    js_task_1 task_1

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -7,6 +7,27 @@
  */
 
 function numeralSum(number) {
+    if (isNaN(number)) {
+        return 0
+    }
+    if(!isFinite(number)){
+        return number
+    }
+
+    let num = number.toString().split('');
+    let sum = 0;
+    console.log(num);
+
+    num.forEach((item) => {
+        if (item === '-' || item === '.' || isNaN(parseInt(item))) {
+            item = 0;
+        }
+
+        let itemNum = Number(item);
+        sum += itemNum;
+
+    });
+    return sum;
 }
 
 module.exports.numeralSum = numeralSum;
