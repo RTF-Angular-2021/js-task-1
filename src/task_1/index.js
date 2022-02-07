3758876c91fff4901e872d7f06aa1e4a1c5e2152
Kirill <fatykovkirill@gmail.com>
2022-02-07 21:51:48 +0500

    Исправление 1 задачи

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -8,11 +8,16 @@
 
 function numeralSum(number) {
     let sum = 0;
+    if (number < 0) {
+        number *= -1
+    }
+    if (number == Infinity) {
+        return Infinity
+    }
     while (number > 0) {
         sum += number % 10
         number = Math.floor(number / 10)
     }
     return sum;
 }
-
 module.exports.numeralSum = numeralSum;
